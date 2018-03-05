c1ac6186ec77fbeca0b587e494ebeaec901cba80
oboehm <o.boehm@optica.de>
2018-03-05 07:53:07 +0100

    all tests for ResourceWalker tranfered from PatternTesting

--- monitor/src/test/java/clazzfish/monitor/internal/ResourceWalkerTest.java
+++ monitor/src/test/java/clazzfish/monitor/internal/ResourceWalkerTest.java
@@ -17,6 +17,7 @@
  */
 package clazzfish.monitor.internal;
 
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,11 +28,12 @@ import java.util.Collection;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * Unit tests for {@link ResourceWalker} class.
  */
-public class ResourceWalkerTest {
+class ResourceWalkerTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(ResourceWalker.class);
     private static final File CLASSES_DIR = new File("target", "test-classes");
@@ -47,6 +49,20 @@ public class ResourceWalkerTest {
         Collection<String> resources = getResourcesFrom(walker);
         assertThat(resources, hasItem("/log4j2.xml"));
     }
+
+    /**
+     * Test method for {@link ResourceWalker#getResources()}, but now without
+     * an special file filter.
+     *
+     * @throws IOException the io exception
+     */
+    @Test
+    public void testGetAllResources() throws IOException {
+        ResourceWalker walker = new ResourceWalker(CLASSES_DIR);
+        Collection<String> resources = getResourcesFrom(walker);
+        assertThat(resources, not(hasItem("/META-INF")));
+        assertThat(resources, hasItem("/log4j2.xml"));
+    }
     
     private static Collection<String> getResourcesFrom(ResourceWalker walker) throws IOException {
         Collection<String> resources = walker.getResources();
@@ -56,6 +72,20 @@ public class ResourceWalkerTest {
             assertThat(rsc, not(endsWith(".class")));
         }
         return resources;
+    }
+
+    /**
+     * Here we test if we can get the directories only.
+     *
+     * @throws IOException the io exception
+     */
+    @Test
+    public void testGetDirResources() throws IOException {
+        ResourceWalker walker = new ResourceWalker(CLASSES_DIR, DirectoryFileFilter.DIRECTORY);
+        Collection<String> packages = walker.getPackages();
+        assertThat(packages, hasItem("clazzfish/"));
+        assertFalse(packages.contains(null), "contain null values: " + packages);
+        LOG.info("{} packages found: {}", packages.size(), packages);
     }
 
 }
