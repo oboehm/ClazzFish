bb42b86e6eec8a05308c6bbda91cef276897384d
oboehm <o.boehm@optica.de>
2018-11-16 22:01:08 +0100

    tests for ConnectorMonitor completed

--- jdbc/src/main/java/clazzfish/jdbc/ConnectionMonitor.java
+++ jdbc/src/main/java/clazzfish/jdbc/ConnectionMonitor.java
@@ -39,7 +39,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * This is the monitor class for the {@link ProxyConnection} which monitors the
- * different newInstance() and close() calls. So you can ask this class how many
+ * different open() and close() calls. So you can ask this monitor how many
  * connections and which connections are open.
  * <p>
  * The instance of this class is automatically registered as MBean as soon as a
--- jdbc/src/test/java/clazzfish/jdbc/ConnectionMonitorTest.java
+++ jdbc/src/test/java/clazzfish/jdbc/ConnectionMonitorTest.java
@@ -17,12 +17,23 @@
  */
 package clazzfish.jdbc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import javax.management.openmbean.OpenDataException;
+import javax.management.openmbean.TabularData;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Unit tests for {@link ConnectionMonitor}.
  */
-public class ConnectionMonitorTest extends AbstractDbTest {
+class ConnectionMonitorTest extends AbstractDbTest {
     
     private static final ConnectionMonitor monitor = ConnectionMonitor.getInstance();
 
@@ -34,6 +45,105 @@ public class ConnectionMonitorTest extends AbstractDbTest {
     @Override
     protected ConnectionMonitor getObject() {
         return monitor;
+    }
+
+    /**
+     * Test method for {@link ConnectionMonitor#getOpenConnections()}.
+     */
+    @Test
+    public void testGetOpenConnections() {
+        assertEquals(1, monitor.getOpenConnections());
+    }
+
+    /**
+     * Test method for {@link ConnectionMonitor#getLastCallerStacktrace()}.
+     */
+    @Test
+    public void testGetLastCallerStacktrace() {
+        StackTraceElement[] stacktrace = monitor.getLastCallerStacktrace();
+        assertThat(stacktrace, not(emptyArray()));
+    }
+
+    /**
+     * Test method for {@link ConnectionMonitor#getCallerStacktraces()}.
+     *
+     * @throws OpenDataException the open data exception
+     */
+    @Test
+    public void testGetCallerStacktraces() throws OpenDataException {
+        TabularData stacktraces = monitor.getCallerStacktraces();
+        assertEquals(1, stacktraces.size());
+    }
+
+    /**
+     * Test method for {@link ConnectionMonitor#getClosedConnections()} and for
+     * {@link ConnectionMonitor#getSumOfConnections()}.
+     */
+    @Test
+    public void testGetClosedConnections() {
+        int expected = monitor.getSumOfConnections() - monitor.getOpenConnections();
+        int closedConnections = monitor.getClosedConnections();
+        assertEquals(expected, closedConnections);
+        assertThat(closedConnections, greaterThanOrEqualTo(0));
+    }
+
+    /**
+     * Test method for {@link ConnectionMonitor#assertConnectionsClosed()}.
+     */
+    @Test
+    public void testAssertConnectionsClosed() {
+        assertThrows(AssertionError.class, ConnectionMonitor::assertConnectionsClosed);
+    }
+
+    /**
+     * Test method for {@link ConnectionMonitor#getCallerOf(Connection)}.
+     */
+    @Test
+    public void testGetCallerOf() {
+        StackTraceElement caller = ConnectionMonitor.getCallerOf(connection);
+        assertEquals(caller.getMethodName(), "setUpConnection", "wrong caller: " + caller);
+    }
+
+    /**
+     * Test method for {@link ConnectionMonitor#getCallerOf(Connection)}. We
+     * do not want to see the {@link ConnectionMonitor} as caller!
+     *
+     * @throws SQLException the SQL exception
+     */
+    @Test
+    public void testGetCallerOfMonitoredConnection() throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:hsqldb:mem:testdb");
+        try (Connection proxyCon = ConnectionMonitor.getMonitoredConnection(con)) {
+            StackTraceElement callerOf = ConnectionMonitor.getCallerOf(con);
+            assertEquals(this.getClass().getName(), callerOf.getClassName());
+        }
+    }
+
+    /**
+     * Test method for {@link ConnectionMonitor#getCallers()}.
+     */
+    @Test
+    public void testGetCallers() {
+        StackTraceElement[] callers = monitor.getCallers();
+        assertEquals(1, callers.length);
+    }
+
+    /**
+     * Test method for {@link ConnectionMonitor#logCallerStacktraces()}. Watch
+     * the log to see if it works.
+     */
+    @Test
+    public void testLogStacktraces() {
+        monitor.logCallerStacktraces();
+    }
+
+    /**
+     * Watch the log to see if the {@link ConnectionMonitor#run()} produces
+     * the expected output.
+     */
+    @Test
+    public void testRun() {
+        monitor.run();
     }
 
 }
