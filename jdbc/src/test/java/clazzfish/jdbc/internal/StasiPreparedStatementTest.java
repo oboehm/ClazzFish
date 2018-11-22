a6fdc6ffe936bdc0c6e3e251eeb2c3e9c2f7d83a
oboehm <o.boehm@optica.de>
2018-11-22 08:26:38 +0100

    unit tests for internal package added

---
+++ jdbc/src/test/java/clazzfish/jdbc/internal/StasiPreparedStatementTest.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2014-2018 by Oliver Boehm
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express orimplied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * (c)reated 05.04.2014 by oliver (ob@oasd.de)
+ */
+
+package clazzfish.jdbc.internal;
+
+import clazzfish.jdbc.AbstractDbTest;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for {@link StasiPreparedStatement} class.
+ *
+ * @author oliver
+ */
+public class StasiPreparedStatementTest extends AbstractDbTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StasiPreparedStatementTest.class);
+
+    /**
+     * Returns an object for testing.
+     *
+     * @return the object
+     */
+    @Override
+    protected PreparedStatement getObject() {
+        try {
+            return this.proxy.prepareStatement("SELECT * FROM country");
+        } catch (SQLException sex) {
+            throw new UnsupportedOperationException("cannot provide object for testing", sex);
+        }
+    }
+
+    /**
+     * Test method for {@link StasiPreparedStatement#execute(String)}.
+     *
+     * @throws SQLException the sQL exception
+     */
+    @Test
+    public void testExecuteUpdate() throws SQLException {
+        try (PreparedStatement stmt = this.proxy
+                .prepareStatement("INSERT INTO country (lang, name, callingcode) " + "VALUES(?, ?, ?)")) {
+            assertEquals(StasiPreparedStatement.class, stmt.getClass());
+            stmt.setString(1, "de");
+            stmt.setString(2, "Germany");
+            stmt.setInt(3, 49);
+            int ret = stmt.executeUpdate();
+            assertEquals(1, ret);
+        }
+    }
+
+    /**
+     * Test method for {@link StasiPreparedStatement#execute(String)}.
+     * If the exceution fails we expect the wrong SQL as part of the
+     * {@link SQLException}.
+     *
+     * @throws SQLException the sQL exception
+     */
+    @Test
+    public void testExecuteFailing() throws SQLException {
+        try (PreparedStatement stmt = this.proxy
+                .prepareStatement("INSERT INTO country (lang, name, callingcode) " + "VALUES(?, ?, ?)")) {
+            stmt.setInt(1, 44);
+            stmt.executeUpdate();
+            fail("SQLException expected");
+        } catch (SQLException expected) {
+            LOG.debug("SQLException expected", expected);
+            String msg = expected.getMessage();
+            assertThat(msg, containsString("INSERT INTO country"));
+        }
+    }
+
+    /**
+     * Test method for {@link StasiPreparedStatement#executeQuery()} and
+     * {@link StasiPreparedStatement#getWrappedPreparedStatement()}.
+     *
+     * @throws SQLException the SQL exception
+     */
+    @Test
+    public void testExcecuteQuery() throws SQLException {
+        try (StasiPreparedStatement stmt = (StasiPreparedStatement) this.proxy
+                .prepareStatement("SELECT * FROM persons WHERE id = ?")) {
+            checkExcecuteQuery(stmt);
+            checkExcecuteQuery(stmt.getWrappedPreparedStatement());
+        }
+    }
+
+    private static void checkExcecuteQuery(PreparedStatement stmt) throws SQLException {
+        stmt.setInt(1, 1001);
+        try (ResultSet query = stmt.executeQuery()) {
+            assertTrue(query.next(), "result expected");
+            assertEquals(1001, query.getInt("id"));
+        }
+    }
+
+    /**
+     * The toString implementation should contain the real SQL.
+     *
+     * @throws SQLException the sQL exception
+     */
+    @Test
+    public void testToStringWithSQL() throws SQLException {
+        try (PreparedStatement stmt = this.proxy
+                .prepareStatement("SELECT * FROM country WHERE lang = ? OR callingcode = ?")) {
+            stmt.setString(1, "de");
+            stmt.setInt(2, 42);
+            String sql = stmt.toString();
+            assertEquals("SELECT * FROM country WHERE lang = 'de' OR callingcode = 42", sql);
+            LOG.info("sql = \"{}\"", sql);
+        }
+    }
+
+}
+
