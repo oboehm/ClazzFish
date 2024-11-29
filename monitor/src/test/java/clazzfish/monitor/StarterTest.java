1f00f85a1542f506507567632b8ae50aa0135b1c
Oli B <o.boehm@optica.de>
2024-11-29 13:55:03 +0100

    broken test fixed

--- monitor/src/test/java/clazzfish/monitor/StarterTest.java
+++ monitor/src/test/java/clazzfish/monitor/StarterTest.java
@@ -31,8 +31,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class StarterTest {
 
     @Test
-    void testRegistration() throws ClassNotFoundException {
-        assertEquals(Starter.class, Class.forName(Starter.class.getName()));
+    void testRegistration() {
+        Starter.main();
         assertTrue(MBeanHelper.isRegistered("clazzfish:type=monitor,name=ClasspathMonitor"));
         assertTrue(MBeanHelper.isRegistered("clazzfish:type=monitor,name=ResourcepathMonitor"));
         assertTrue(MBeanHelper.isRegistered("clazzfish.monitor:name=ClazzRecorder,type=rec"));
