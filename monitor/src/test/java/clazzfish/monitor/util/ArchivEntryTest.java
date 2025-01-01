538fe8870ca091b3f520716c4ca655883864057f
Oli B <boehm@javatux.de>
2025-01-01 15:02:48 +0100

    tests updated for updated libs

--- monitor/src/test/java/clazzfish/monitor/util/ArchivEntryTest.java
+++ monitor/src/test/java/clazzfish/monitor/util/ArchivEntryTest.java
@@ -72,7 +72,7 @@ public class ArchivEntryTest {
         FilenameUtils.separatorsToUnix(Environment.getLocalMavenRepositoryDir().getPath());
         File logDir = new File(mavenRepositoryDir, "commons-logging/commons-logging");
         log0jar = new File(logDir, "1.1/commons-logging-1.1.jar");
-        log1jar = new File(logDir, "1.1.1/commons-logging-1.1.1.jar");
+        log1jar = new File(logDir, "1.3.4/commons-logging-1.3.4.jar");
     }
 
     /**
@@ -350,7 +350,7 @@ public class ArchivEntryTest {
      */
     @Test
     public void testArchivEntry() throws URISyntaxException, IOException {
-        File jarFile = new File(mavenRepositoryDir, "org/apache/commons/commons-lang3/3.12.0/commons-lang3-3.12.0.jar");
+        File jarFile = new File(mavenRepositoryDir, "org/apache/commons/commons-lang3/3.17.0/commons-lang3-3.17.0.jar");
         String resource = "org/apache/commons/lang3/StringUtils.class";
         ArchivEntry entry = getArchivEntry(jarFile, resource);
         LOG.info("entry=" + entry + ", size=" + entry.getSize());
