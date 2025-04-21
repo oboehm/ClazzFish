84b69bd6c03e353fddb584266d521ee70f9fb5e7
Oli B <boehm@javatux.de>
2025-04-21 20:25:51 +0200

    Release 2.6

--- src/script/deploy.sh
+++ src/script/deploy.sh
@@ -8,7 +8,7 @@
 
 # set up some constants
 URL=https://oss.sonatype.org/service/local/staging/deploy/maven2/
-VERSION=2.5.2
+VERSION=2.6.0
 options="gpg:sign-and-deploy-file -Durl=$URL -DrepositoryId=sonatype-nexus-staging"
 
 # passphrase is needed for signing
@@ -44,3 +44,4 @@ deploy_pom_for clazzfish
 deploy_jar_for monitor clazzfish-monitor
 deploy_jar_for agent clazzfish-agent
 deploy_jar_for jdbc clazzfish-jdbc
+deploy_jar_for spi/git clazzfish-spi-git
